33d02df18e173d6bb726e69f572ebd945f9fa246
Admin <Admin@10.10.7.77>
2020-11-11 15:28:03 +0530

    urls

---
+++ src/test/java/TestExecute/Hydroflask/URLS/URLSCheck.java
@@ -0,0 +1,47 @@
+package TestExecute.Hydroflask.URLS;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.HydroHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class URLSCheck {
+	
+	String datafile = "Hydroflask//hydroURL.xlsx";	
+	HydroHelper Hydro=new HydroHelper(datafile);
+  @Test
+  public void URL_Navigation() {
+	  try {
+		//  String URL="https://hydro-stg-m2.heledigital.com/featured/new-arrivals";
+	      Hydro.new_arrivals("featured");
+	      Hydro.trail_Series("featured");
+	      Hydro.limited_edition("featured");
+	        
+		}
+		catch (Exception e) {
+			
+			Assert.fail(e.getMessage(), e);
+		} 
+}
+	  
+	  
+  
+  
+  @BeforeTest
+  public void startTest() throws Exception {
+	 System.setProperty("configFile", "Hydroflask\\config.properties");
+	  Login.signIn();
+	  Hydro.acceptPrivecy();
+	  
+  }
+  @AfterTest
+	public void clearBrowser()
+	{
+	Common.closeAll();
+
+	}
+}
