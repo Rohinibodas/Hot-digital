79c027a224e17d13bd409d02b6cf89cc03a7b2bd
Admin <Admin@10.10.7.111>
2020-11-17 13:42:21 +0530

    search

---
+++ src/test/java/TestExecute/Hydroflask/Smoke/SearchProduct.java
@@ -0,0 +1,43 @@
+package TestExecute.Hydroflask.Smoke;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.HydroHelper;
+import TestLib.Login;
+
+public class SearchProduct {
+
+	String datafile = "Hydroflask//HydroTestData.xlsx";	
+	HydroHelper Hydro=new HydroHelper(datafile);
+  @Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+  public void searchProduct() {
+	  try {
+	      Hydro.SampleSearchProduct("hydro-flask-small-tag-along-bottle-sling-brick");
+	        
+	        
+		}
+		catch (Exception e) {
+			
+			Assert.fail(e.getMessage(), e);
+		} 
+	}
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+		
+    // Common.closeAll();
+
+	}
+	
+	@BeforeTest
+	  public void startTest() throws Exception {
+		 System.setProperty("configFile", "Hydroflask\\config.properties");
+		  Login.signIn();
+		  Hydro.acceptPrivecy();
+		  
+	  }
+}
