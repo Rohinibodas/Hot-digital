571c75a1b2f24934e4990f3646176a89fbae8cfb
Harish-lotus <harish@lotuswave.net>
2020-12-14 10:03:08 +0530

    Revlon UK System Test Cases for Mobile & Web

---
+++ src/test/java/TestExecute/RevlonUK/SmokeTC/RHT_ST_GuestUserCheckout_InvalidCreditCard.java
@@ -0,0 +1,59 @@
+package TestExecute.RevlonUK.SmokeTC;
+
+import org.testng.annotations.Test;
+
+import TestComponent.RevlonUk.RevlonUKHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+
+public class RHT_ST_GuestUserCheckout_InvalidCreditCard {
+	String datafile = "revlonUK//RevlonUKTestData.xlsx";	
+	RevlonUKHelper revlon=new RevlonUKHelper(datafile);
+	
+	
+	@Test(priority=1)
+  public void GuestUserCheckout() {
+		try {
+			revlon.SearchProduct("productName");
+			revlon.Productselection();
+			revlon.navigateMinicart();
+			revlon.checkoutPage();
+			revlon.navigateCheckoutGuest("Guest_shipping");
+			revlon.invalidCreditCard("InvalidCreditCard");
+		}
+		catch (Exception e) {
+			
+			Assert.fail(e.getMessage(), e);
+		} 
+		
+	  
+  }
+	@BeforeMethod
+	@Parameters({"browser"}) 
+	  public void startTest(String browser) throws Exception {
+		System.setProperty("configFile", "RevlonUK\\config.properties");
+		  Login.signIn(browser);
+		  
+	  }
+	
+	/*@BeforeMethod
+	@Parameters({"browser"})  
+	  public void startTest() throws Exception {
+		System.setProperty("configFile", "RevlonUK\\config.properties");
+		  Login.signIn("chrome");
+		  
+	  }*/
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+		Common.closeAll();
+	}
+
+}
