7313df0487d787ce3d527f71fcde5a3657b6e13f
pamsvc_mkoppanadam <pamsvc_mkoppanadam@AWS-KOPPMANO.helenoftroy.com>
2021-01-13 10:12:47 +0000

    C

---
+++ src/test/java/TestExecute/DryBar/ApplyGiftCardonCheckoutPage.java
@@ -0,0 +1,48 @@
+package TestExecute.DryBar;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.DryBar.DryBarHelper;
+import TestLib.Login;
+
+public class ApplyGiftCardonCheckoutPage {
+	String datafile = "DryBar//DryBarTestData.xlsx";	
+	DryBarHelper drybar=new DryBarHelper(datafile);
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+  public void applyGiftCardonCheckoutPage() throws Exception {
+	//  drybar.clickMyaccount();
+	  
+	  drybar.clickHairProducts();
+	  drybar.selectproduct("ProductName");
+	  drybar.increaseProductQuantity("2");
+	  drybar.clickAddtoBag();
+	  drybar.clickminiCartButton();
+	  drybar.clickCheckoutButton();
+	  drybar.click_GuestCheckOut();
+	  drybar.guestShippingAddress("ShippingAddress");
+	  drybar.gitCard();
+	  drybar.click_place_order_button();
+	//  drybar.Click_PaymetricPaymentMethod();
+	 // drybar.Edit_BillingAddress_PaymetricPaymentMethod("BiillingAddress");
+	  //drybar.creditCard_payment("PaymentDetails");
+  }
+  
+	//ul[contains(@class,'header links')]/li[2]/a
+  
+  @AfterTest
+	public void clearBrowser()
+	{
+		//Common.closeAll();
+
+	}
+	
+	@BeforeTest
+	  public void startTest() throws Exception {
+		 System.setProperty("configFile", "DryBar\\config.properties");
+		  Login.signIn();
+		 
+		  
+	  }
+}
---
+++ src/test/java/TestExecute/DryBar/ApplyOfferCodeonCheckoutPage.java
@@ -0,0 +1,41 @@
+package TestExecute.DryBar;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.DryBar.DryBarHelper;
+import TestLib.Login;
+
+public class ApplyOfferCodeonCheckoutPage {
+	String datafile = "DryBar//DryBarTestData.xlsx";	
+	DryBarHelper drybar=new DryBarHelper(datafile);
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+  public void f() throws Exception {
+		  drybar.clickHairProducts();
+		  drybar.selectproduct("ProductName");
+		  drybar.increaseProductQuantity("2");
+		  drybar.clickAddtoBag();
+		  drybar.clickminiCartButton();
+		  drybar.clickCheckoutButton();
+		  drybar.click_GuestCheckOut();
+		  drybar.guestShippingAddress("ShippingAddress");
+	 // drybar.select_USPS_StandardGround_shippingMethod();
+  }
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+	//Common.closeAll();
+
+	}
+	
+
+	@BeforeTest
+	  public void startTest() throws Exception {
+		 System.setProperty("configFile", "DryBar\\config.properties");
+		  Login.signIn();
+		 
+		  
+	  }
+}
