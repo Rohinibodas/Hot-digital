0c233b45626833d746d73d9fc1eacde7d86aacb5
Harish-lotus <harish@lotuswave.net>
2021-01-19 09:35:25 +0530

    Revlon UK Content Site automation Test cases

---
+++ src/test/java/TestExecute/RevlonUK/Sprint1Mob/RUK_MOB_ST_GuestUserPDPBootsHatch.java
@@ -0,0 +1,55 @@
+package TestExecute.RevlonUK.Sprint1Mob;
+
+import org.testng.annotations.Test;
+
+import TestComponent.RevlonUk.RevlonUKMobileHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+
+public class RUK_MOB_ST_GuestUserPDPBootsHatch {
+	String datafile = "revlonUK//RevlonUKTestData.xlsx";	
+	RevlonUKMobileHelper revlon=new RevlonUKMobileHelper(datafile);
+	
+	
+	@Test(priority=1)
+	public void GuestUserBootsHatch() throws Exception {
+		try {
+			revlon.SearchProduct("productName");
+			revlon.Productselection();
+			revlon.BootsHatchproduct();
+		}
+		catch (Exception e) {
+			
+			Assert.fail(e.getMessage(), e);
+		} 
+	}
+	
+	@BeforeMethod
+	@Parameters({"device"})  
+	  public void startTest(String Device) throws Exception {
+		System.setProperty("configFile", "RevlonUK\\config.properties");
+		  Login.signIn("chrome",Device);
+		  
+	  }
+	
+	/*@BeforeMethod
+    //@Parameters({"device"})
+      public void startTest() throws Exception {
+        System.setProperty("configFile", "RevlonUK\\config.properties");
+          Login.signIn("chrome","iPhone X");
+         
+      }*/
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+		Common.closeAll();
+	}
+
+}
