b42a9f731f77ee2454f6a37abcf7d50ba9b594d7
admin <admin@10.10.7.109>
2021-03-04 15:44:17 +0530

    Honeywell

---
+++ src/test/java/TestExecute/Honeywell/AccountCreation.java
@@ -0,0 +1,37 @@
+package TestExecute.Honeywell;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Honeywell.Honeywellhelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class AccountCreation {
+	String datafile = "Honeywell\\HoneywellTestData.xlsx";	
+	Honeywellhelper honeyWell=new Honeywellhelper(datafile);
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+  
+  public void accountcreation() {
+		
+		
+		honeyWell.createaccount("AccountDetails");
+  }
+	
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+	//Common.closeAll();
+
+	}
+	
+	@BeforeTest
+	  public void startTest() throws Exception {
+		 System.setProperty("configFile", "Honeywell\\config.properties");
+		  Login.signIn();
+		 
+		  
+	  }
+}
---
+++ src/test/java/TestExecute/Honeywell/RegisteredUser_Checkout_CreditCard_VISA.java
@@ -0,0 +1,56 @@
+
+package TestExecute.Honeywell;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Honeywell.Honeywellhelper;
+
+import TestLib.Common;
+import TestLib.Login;
+
+public class RegisteredUser_Checkout_CreditCard_VISA {
+	String datafile = "Honeywell\\HoneywellTestData.xlsx";	
+	Honeywellhelper honeyWell=new Honeywellhelper(datafile);
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	
+	public void registeredUser_Checkout_CreditCard_VISA() throws Exception {
+
+		try {
+			honeyWell.loginHoneywell("AccountDetails");
+			honeyWell.verifyingHomePage();
+			honeyWell.click_Airpurifiers();
+			honeyWell.adding_product_toCart("productnameRegester");
+			honeyWell.clickAddtoBag();
+			honeyWell.clickminicartButton();
+			honeyWell.clickminicartcheckout();
+			honeyWell.addDeliveryAddress_registerUser("ShippingAddress");
+			honeyWell.creditCard_payment("ccamex");
+			honeyWell.order_Verifying();
+		}
+		catch (Exception e) {
+			
+			Assert.fail(e.getMessage(), e);
+		} 
+	}
+	
+	
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+	//Common.closeAll();
+
+	}
+	
+	@BeforeTest
+	  public void startTest() throws Exception {
+		 System.setProperty("configFile", "Honeywell\\config.properties");
+		  Login.signIn();
+		 
+		  
+	  }
+
+}
