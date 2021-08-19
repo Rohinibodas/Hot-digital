0079501d3015a99c60da94da5f8fcf4a29778c25
cent_mkoppanadam <cent_mkoppanadam@helenoftroy.com>
2021-08-19 05:59:44 -0600

    Getuserfile

--- src/test/java/TestExecute/Hydroflask/Smoke/Guest_Checkout_CreditCard.java
+++ src/test/java/TestExecute/Hydroflask/Smoke/Guest_Checkout_CreditCard.java
@@ -1,7 +1,6 @@
 package TestExecute.Hydroflask.Smoke;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -23,49 +22,33 @@ public class Guest_Checkout_CreditCard {
 		try {
 
 			Hydro.orderSubmit("Bottles");
-			Hydro.viewcart();
-			HashMap<String, HashMap<String, String>> productinfromation = Hydro.productinfromation();
-			Hydro.checkOut();
-			HashMap<String, String> shippingaddresssf = Hydro.addDeliveryAddress_validation("Address");
-			HashMap<String, String> ordertaxammount = Hydro.orderamountinfromation();
-			HashMap<String, String> paymentdetriles = Hydro.add_PaymentDetails("PaymentDetails");
-			String order = Hydro.verifyingSucesspage();
+			 Hydro.checkOut();
+		       Hydro.addDeliveryAddress("Address");
+			   Hydro.updatePaymentAndSubmitOrder("PaymentDetails");
+		        
+				
+			}
+			catch (Exception e) {
+				
+				Assert.fail(e.getMessage(), e);
+			} 
+	  }
+	  
+	  @AfterTest
+		public void clearBrowser()
+		{
+	 Common.closeAll();
 
-			Hydro.HydroAdminlogin("AdminLogins");
-			Hydro.selectManulExport(order);
-
-			/*
-			 * 
-			 */
-			String TrantionID=Hydro.seleOrderoprtion(order);
-			
-			  Hydro.productinfromationvalidation(productinfromation, order);
-			  Hydro.shippingvalidaing_GustUserXML(shippingaddresssf, order);
-			  
-			  Hydro.TotalvalidationXML(ordertaxammount, order);
-			  Hydro.Billingaddress_GustUserXML_Validation(shippingaddresssf,order);
-			  Hydro.card_details_validationXML(paymentdetriles, order);
-			  Hydro.ValidatingTranctionIDInXML(order, TrantionID);
-			  //Hydro.shippingvalidaingXML("Address","4000421161");
-			 
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.fail(e.getMessage(), e);
 		}
+		
+	  
+	  
+	  @BeforeTest
+	  public void startTest() throws Exception {
+		 System.setProperty("configFile", "Hydroflask\\config.properties");
+		  Login.signIn();
+		  Hydro.acceptPrivecy();
+		  
+	  }
+
 	}
-
-	@AfterTest
-	public void clearBrowser() {
-		 Common.closeAll();
-
-	}
-
-	@BeforeTest
-	public void startTest() throws Exception {
-		System.setProperty("configFile", "Hydroflask\\config.properties");
-		Login.signIn();
-		Hydro.acceptPrivecy();
-		Hydro.ClosADD();
-	}
-
-}
